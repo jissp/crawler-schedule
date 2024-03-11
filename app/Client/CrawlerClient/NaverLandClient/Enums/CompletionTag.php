1e84f3ba230a412bf7f19063548009633bbf4a7d
jissp <jissps@naver.com>
2024-03-11 23:07:04 +0900

    fix: 수집 조건 변경

--- app/Client/CrawlerClient/NaverLandClient/Enums/CompletionTag.php
+++ app/Client/CrawlerClient/NaverLandClient/Enums/CompletionTag.php
@@ -6,4 +6,5 @@ enum CompletionTag: string
 {
     /** 4년 이내 */
     case COMPLETION4UNDER = 'COMPLETION4UNDER';
+    case COMPLETION10UNDER = 'COMPLETION10UNDER';
 }
---
+++ app/Client/CrawlerClient/NaverLandClient/Enums/HouseHoldTag.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Client\CrawlerClient\NaverLandClient\Enums;
+
+enum HouseHoldTag: string
+{
+    case HSEH100 = 'HSEH100';
+}
